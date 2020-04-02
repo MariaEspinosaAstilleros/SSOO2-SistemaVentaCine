81291d7ab3c092f138ae953d72aabb713899aff2
MariaEspinosaAstilleros <maria.espinosa6@alu.uclm.es>
2020-04-02 18:16:34 +0200

    First commit

---
+++ include/msgRequest.h
@@ -0,0 +1,12 @@
+/******************************************************
+ * Project:         Práctica 3 de Sistemas Operativos II
+ * 
+ * Program name:    msgRequest.h
+ 
+ * Author:          María Espinosa Astilleros
+ * 
+ * Date created:    2/4/2020
+ * 
+ * Purpose:         Contain the definitions of the class for the requests
+ * 
+ ******************************************************/
---
+++ src/cinemaSale.cpp
@@ -0,0 +1,12 @@
+/******************************************************
+ * Project:         Práctica 3 de Sistemas Operativos II
+ * 
+ * Program name:    cinemaSale.cpp
+ 
+ * Author:          María Espinosa Astilleros
+ * 
+ * Date created:    2/4/2020
+ * 
+ * Purpose:         
+ * 
+ ******************************************************/
