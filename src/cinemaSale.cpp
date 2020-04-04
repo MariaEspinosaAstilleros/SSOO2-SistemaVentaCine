f74ae52d1cb4894e95630da81545b1e85bc98d22
MariaEspinosaAstilleros <maria.espinosa6@alu.uclm.es>
2020-04-04 12:59:54 +0200

    data structures added

--- src/cinemaSale.cpp
+++ src/cinemaSale.cpp
@@ -15,7 +15,66 @@
 #include <queue>
 #include <vector>
 #include <mutex> 
+#include <thread> 
 #include <condition_variable> 
 
 #include "../include/color.h"
 #include "../include/msgRequest.h"
+#include "../include/SemCounter.h"
+
+#define NUM_SEATS   72
+#define NUM_SP      3
+#define NUM_CLIENTS 20
+
+/*Globals variables*/
+int turn = -1; 
+
+/*Messages queue*/
+std::queue<std::thread>                 g_queue_tickets;            /*queue of clients to buy tickets*/
+std::queue<std::thread>                 g_queue_drinkpop;           /*queue of clients to buy drinks and popcorns*/
+std::queue<std::thread>                 g_queue_cinema;             /*queue representing cinema*/
+std::queue<MsgRequestTickets*>          g_queue_request_tickets;    /*queue to request tickets*/
+std::queue<MsgRequestSalePoint*>        g_queue_request_pv;         /*queue to request sale point*/
+std::queue<MsgRequestStock*>            g_queue_request_stock;      /*queue to request thread stocker*/
+std::priority_queue<MsgRequestPay*>     g_queue_request_pay;        /*queue to request pay*/
+
+/*Vectors*/
+std::vector<std::thread>                g_v_sale_point;              /*vector with id of sale point*/
+
+/*Semaphores*/
+std::mutex                              g_sem_tickets;              /*sem to wait tickets*/
+std::mutex                              g_sem_toffice;              /*sem to wake ticket office*/
+std::mutex                              g_sem_seats;                /*sem to control seats*/
+
+/*Condition variable*/
+std::condition_variable                 g_cv_ticket_office;         /*condition variable to notify the turn of ticket office*/
+std::condition_variable                 g_cv_sale_point;            /*condition variable to notify the turn of sale point*/
+std::condition_variable                 g_cv_client_attended;       /*conditional variable to notify the client that it has been attended*/
+std::condition_variable                 g_cv_pay;                   /*condition variable to notify the turn to pay*/
+std::condition_variable                 g_cv_stock_attended;        /*conditional variable to notify the sale point that it has been replanished*/
+
+/*Functions declaration*/
+int generateRandomNumber(int lim); 
+
+
+
+
+/******************************************************
+ * Function name:    main
+ * Date created:     4/4/2020
+ * Input arguments: 
+ * Purpose:          Principal method of class 
+ * 
+ ******************************************************/
+int main(int argc, char *argv[]){
+
+}
+
+/******************************************************
+ * Function name:    generateRandomNumber
+ * Date created:     4/4/2020
+ * Input arguments: 
+ * Purpose:          Generate a random number 
+ * 
+ ******************************************************/
+int generateRandomNumber(int lim){ return (rand()%(lim-1))+1; }
